db06cc7a1ee73d3cc7f79cae6f2529c751fef8de
vtu30003-gif <vtu30003@veltech.edu.in>
2026-09-15 14:59:20 +0530

    ✅ Binary Tree Level Order Traversal | Medium | Java | Runtime: 1 ms (95.89%) | Memory: 46.9 MB | 2026-09-15

---
+++ leetcode/medium/0102-binary-tree-level-order-traversal/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+
+                if (currentNode.left != null) queue.add(currentNode.left);
+                if (currentNode.right != null) queue.add(currentNode.right);
+            }
+
+            result.add(currentLevel);
+        }
+
+        return result;
+    }
+}
