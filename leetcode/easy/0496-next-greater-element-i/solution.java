bfae52e9badab6ecb4990fbc6bc67b9837034f44
vtu30003-gif <vtu30003@veltech.edu.in>
2026-09-15 14:40:38 +0530

    ✅ Next Greater Element I | Easy | Java | Runtime: 3 ms (92.98%) | Memory: 45.3 MB | 2026-09-15

---
+++ leetcode/easy/0496-next-greater-element-i/solution.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        // Process nums2 using monotonic stack
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        // Build the final result for nums1
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
+    }
+}
