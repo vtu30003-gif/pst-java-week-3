280d15b3413fca06a80f9fc595b0382bf4cfa48a
vtu30003-gif <vtu30003@veltech.edu.in>
2026-09-15 14:41:43 +0530

    ✅ Daily Temperatures | Medium | Java | Runtime: 23 ms (92.61%) | Memory: 104.2 MB | 2026-09-15

---
+++ leetcode/medium/0739-daily-temperatures/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>(); // Stores indices
+
+        for (int i = 0; i < n; i++) {
+            // While current temperature is warmer than the temperature at stack's top index
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
